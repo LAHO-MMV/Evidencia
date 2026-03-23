e85fc51e04e1febe689d8c4592975dfce749dc82
LAHO-MMV <a01713527@tec.mx>
2026-03-22 18:51:44 -0600

    cumplimiento normas segun flake8

--- pacman.py
+++ pacman.py
@@ -1,30 +1,21 @@
-"""Pacman, classic arcade game.
-
-Exercises
-
-1. Change the board.
-2. Change the number of ghosts.
-3. Change where pacman starts.
-4. Make the ghosts faster/slower.
-5. Make the ghosts smarter.
-"""
-
 from random import choice
-from turtle import *
+import turtle
 
 from freegames import floor, vector
 
 state = {'score': 0}
-path = Turtle(visible=False)
-writer = Turtle(visible=False)
+path = turtle.Turtle(visible=False)
+writer = turtle.Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
+
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(10, 0)],
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
 ]
+
 # fmt: off
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -58,7 +49,7 @@ def square(x, y):
     path.down()
     path.begin_fill()
 
-    for count in range(4):
+    for _ in range(4):
         path.forward(20)
         path.left(90)
 
@@ -69,8 +60,7 @@ def offset(point):
     """Return offset of point in tiles."""
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
-    index = int(x + y * 20)
-    return index
+    return int(x + y * 20)
 
 
 def valid(point):
@@ -90,7 +80,7 @@ def valid(point):
 
 def world():
     """Draw world using path."""
-    bgcolor('black')
+    turtle.bgcolor('black')
     path.color('blue')
 
     for index in range(len(tiles)):
@@ -104,7 +94,7 @@ def world():
             if tile == 1:
                 path.up()
                 path.goto(x + 10, y + 10)
-                path.dot(2, 'white')
+                path.dot(6, 'green')  # alimento modificado
 
 
 def move():
@@ -112,7 +102,7 @@ def move():
     writer.undo()
     writer.write(state['score'])
 
-    clear()
+    turtle.clear()
 
     if valid(pacman + aim):
         pacman.move(aim)
@@ -122,39 +112,40 @@ def move():
     if tiles[index] == 1:
         tiles[index] = 2
         state['score'] += 1
+
         x = (index % 20) * 20 - 200
         y = 180 - (index // 20) * 20
         square(x, y)
 
-    up()
-    goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow')
+    turtle.up()
+    turtle.goto(pacman.x + 10, pacman.y + 10)
+    turtle.dot(20, 'yellow')
 
     for point, course in ghosts:
         if valid(point + course):
             point.move(course)
         else:
             options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
             ]
             plan = choice(options)
             course.x = plan.x
             course.y = plan.y
 
-        up()
-        goto(point.x + 10, point.y + 10)
-        dot(20, 'red')
+        turtle.up()
+        turtle.goto(point.x + 10, point.y + 10)
+        turtle.dot(20, 'red')
 
-    update()
+    turtle.update()
 
-    for point, course in ghosts:
+    for point, _ in ghosts:
         if abs(pacman - point) < 20:
             return
 
-    ontimer(move, 100)
+    turtle.ontimer(move, 100)
 
 
 def change(x, y):
@@ -164,17 +155,20 @@ def change(x, y):
         aim.y = y
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
+
 writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
-listen()
-onkey(lambda: change(5, 0), 'Right')
-onkey(lambda: change(-5, 0), 'Left')
-onkey(lambda: change(0, 5), 'Up')
-onkey(lambda: change(0, -5), 'Down')
+
+turtle.listen()
+turtle.onkey(lambda: change(5, 0), 'Right')
+turtle.onkey(lambda: change(-5, 0), 'Left')
+turtle.onkey(lambda: change(0, 5), 'Up')
+turtle.onkey(lambda: change(0, -5), 'Down')
+
 world()
 move()
-done()
+turtle.done()
